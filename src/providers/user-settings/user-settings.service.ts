fe5b2e49bc4538233a8a7390f9ef800e47d8653a
Dennis Xiao <dennisboys@gmail.com>
2018-02-17 23:00:08 +1100

    removed stuff

--- src/providers/user-settings/user-settings.service.ts
+++ src/providers/user-settings/user-settings.service.ts
@@ -1,14 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import * as _ from 'lodash';
 
-/*
-  Generated class for the UserSettingsProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class UserSettings {
 
