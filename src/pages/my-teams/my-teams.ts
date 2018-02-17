ad8a8a254d793be81d2f63371c15c89e7e4ce708
Dennis Xiao <dennisboys@gmail.com>
2018-02-17 16:24:53 +1100

    added group to teams

--- src/pages/my-teams/my-teams.ts
+++ src/pages/my-teams/my-teams.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { TournamentsPage } from '../pages';
+import { LoadingController, NavController, NavParams } from 'ionic-angular';
+import { TournamentsPage, TeamHomePage } from '../pages';
+import { EliteApi } from '../../providers/providers';
 
 @Component({
   selector: 'page-my-teams',
@@ -8,11 +9,42 @@ import { TournamentsPage } from '../pages';
 })
 export class MyTeamsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  favorites = [
+    {
+        team: { id: 6182, name: 'HC Elite 7th', coach: 'Michelotti' },
+        tournamentId: '89e13aa2-ba6d-4f55-9cc2-61eba6172c63',
+        tournamentName: 'March Madness Tournament'
+    },
+    {
+        team: { id: 805, name: 'HC Elite', coach: 'Michelotti' },
+        tournamentId: '98c6857e-b0d1-4295-b89e-2d95a45437f2',
+        tournamentName: 'Holiday Hoops Challenge'
+    }
+  ];
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private loadingCtrl: LoadingController,
+    private eliteApi: EliteApi) {
   }
 
   goToTournaments() {
     this.navCtrl.push(TournamentsPage);
+  }
+
+  favTapped($event, favorite) {
+    const loader = this.loadingCtrl.create({
+      content: 'Getting data...',
+      dismissOnPageChange: true
+    });
+
+    loader.present();
+
+    this.eliteApi.getTournamentData(favorite.tournamentId)
+      .subscribe((tournament) => {
+        this.navCtrl.push(TeamHomePage, favorite.team);
+      });
   }
 
 }
--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LoadingController, NavController, NavParams } from 'ionic-angular';
 import { TeamHomePage } from '../pages';
 import { EliteApi } from '../../providers/providers';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-teams',
@@ -9,6 +10,8 @@ import { EliteApi } from '../../providers/providers';
 })
 export class TeamsPage {
 
+  private allTeams: any;
+  private allTeamDivisions: any;
   teams = [];
 
   constructor(
@@ -23,6 +26,11 @@ export class TeamsPage {
     this.navCtrl.push(TeamHomePage, team);
   }
 
+  logger(data) {
+    console.log(data);
+    return data;
+  }
+
   ionViewDidLoad() {
     const selectedTournament = this.navParams.data;
 
@@ -35,7 +43,18 @@ export class TeamsPage {
       // fetch the teams data based on the tournament id
       this.eliteApi.getTournamentData(selectedTournament.id)
         .subscribe(currentTournament => {
-          this.teams = currentTournament.teams;
+          this.allTeams = currentTournament.teams;
+
+          console.log('all teams', this.allTeams);
+          this.allTeamDivisions =
+            _.chain(currentTournament.teams)
+             .groupBy('division')
+             .toPairs()
+             .map(item => _.zipObject(['divisionName', 'divisionTeams'], item))
+             .value();
+
+          this.teams = this.allTeamDivisions;
+
           loader.dismiss();
         });
     });
